514b266c685b37529168d51cf9fcda5d197b1312
Cristian Giovanny Sánchez Pineda <harmaharcri@hotmail.com>
2023-05-07 15:03:08 -0500

    Added the ddl of stations table

--- sql/DDL-TABLES.sql
+++ sql/DDL-TABLES.sql
@@ -1,17 +1,32 @@
-CREATE TABLE tripscsanchezp(TripID INT,
- Duraion INT, 
- StartDate STRING,
- StartStation STRING, 
- StartTerminal INT, 
- EndDate STRING, 
- EndStation STRING, 
- EndTerminal INT, 
- Bike INT, 
- SubscriberType STRING, 
- ZipCode STRING)
-
+CREATE TABLE tripscsanchezp(
+    TripID INT,
+    Duraion INT, 
+    StartDate STRING,
+    StartStation STRING, 
+    StartTerminal INT, 
+    EndDate STRING, 
+    EndStation STRING, 
+    EndTerminal INT, 
+    Bike INT, 
+    SubscriberType STRING, 
+    ZipCode STRING
+)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
 LOCATION '/user/csanchezp/tripscsanchezp/'
+TBLPROPERTIES ("skip.header.line.count"="1");
+
+CREATE TABLE stationscsanchezp(
+    station_id INT, 
+    name STRING, 
+    lat FLOAT,
+    long FLOAT, 
+    dockcount INT, 
+    landmark STRING, 
+    installation STRING
+)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+LOCATION '/user/csanchezp/stationscsanchezp/'
 TBLPROPERTIES ("skip.header.line.count"="1");
 
