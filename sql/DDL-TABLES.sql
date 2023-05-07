7223df0fefd82b6577109c1b544875e3c587c968
Cristian Giovanny Sánchez Pineda <harmaharcri@hotmail.com>
2023-05-07 14:59:19 -0500

    Added the ddl of trips table

--- sql/DDL-TABLES.sql
+++ sql/DDL-TABLES.sql
@@ -1,1 +1,17 @@
-CREATE TABLE tripscsanchezp()
+CREATE TABLE tripscsanchezp(TripID INT,
+ Duraion INT, 
+ StartDate STRING,
+ StartStation STRING, 
+ StartTerminal INT, 
+ EndDate STRING, 
+ EndStation STRING, 
+ EndTerminal INT, 
+ Bike INT, 
+ SubscriberType STRING, 
+ ZipCode STRING)
+
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+LOCATION '/user/csanchezp/tripscsanchezp/'
+TBLPROPERTIES ("skip.header.line.count"="1");
+
